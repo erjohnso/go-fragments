e0614ea0228370c758b6a2057b185c71602596ca
Eric Johnson <erjohnso@coding-zone.com>
2013-04-06 22:47:21 -0700

    finished commaify

--- examples/commaify_number.go
+++ examples/commaify_number.go
@@ -5,38 +5,57 @@ import (
     "strings"
 )
 
-func CommaifyFloat64(fp float64, sep string) string {
+func CommaifyFloat64(fp float64) string {
     var (
         isNegative bool
         strfp string
         parts []string
         result []string
+        l int
     )
     if fp < 0 {
         isNegative = true
         fp = fp * -1
     }
+    // convert to string and trim off leading/trailing zeros
     strfp = strings.Trim(fmt.Sprintf("%f", fp), "0")
+    // split on decimal point
     parts = strings.Split(strfp, ".")
-    if len(parts) == 2 {
-        result = append(result, parts[1])
+    if len(parts) == 2 && len(parts[1]) > 0 {
+        result = append(result, parts[1], ".")
     }
-    for i := len(parts[1]); i > 3; i-=3 {
-        result = append(result, parts[1][i:(i-3)])
+    // is number >= 100?
+    if len(parts[0]) >= 3 {
+        tmp := parts[0]
+        for len(tmp) >= 3 {
+            l = len(tmp)
+            result = append(result, tmp[(l-3):l])
+            tmp = tmp[0:(l-3)]
+        }
+        if len(tmp) > 0 {
+            result = append(result, tmp)
+        }
+    } else {
+        result = append(result, parts[0])
+    }
+    // reverse list
+    for i, j := 0, len(result) - 1; i < j; i, j = i+1, j-1 {
+        result[i], result[j] = result[j], result[i]
     }
     if isNegative {
-        result = append(result, "-")
+        return strings.Replace(
+            fmt.Sprintf("-%s", strings.Join(result, ",")), ",.,", ".", 1)
     }
-    fmt.Println(result)
-    return ""
+    return strings.Replace(
+        fmt.Sprintf("%s", strings.Join(result, ",")), ",.,", ".", 1)
 }
 
 func main() {
-    fmt.Println(CommaifyFloat64(45, ","))
-    fmt.Println(CommaifyFloat64(123456789, ","))
-    fmt.Println(CommaifyFloat64(3456789.789, ","))
-    fmt.Println(CommaifyFloat64(-0123456789.789, ","))
-    fmt.Println(CommaifyFloat64(-10123456789.789, ","))
-    fmt.Println(CommaifyFloat64(00987654321.12345, ","))
-    fmt.Println(CommaifyFloat64(9123456789.0789, ",")) // rounding error .078899
+    fmt.Printf("1. %s\n", CommaifyFloat64(45))
+    fmt.Printf("2. %s\n", CommaifyFloat64(123456789))
+    fmt.Printf("3. %s\n", CommaifyFloat64(34567890.0123))
+    fmt.Printf("4. %s\n", CommaifyFloat64(-0123456789.789))
+    fmt.Printf("5. %s\n", CommaifyFloat64(-10123456789.789))
+    fmt.Printf("6. %s\n", CommaifyFloat64(00987654321.1234500))
+    fmt.Printf("7. %s (rounding error)\n", CommaifyFloat64(9123456789.0789))
 }
